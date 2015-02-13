9f55b0784632f195f682c5c3745e4b5708432eb3
Alessandro Siletto <alessandro@siletto.it>
2015-02-13 17:58:24 +0100

    fix appconfig; howto generate rsa keys

--- src/main/java/it/siletto/ms/auth/AppConfiguration.java
+++ src/main/java/it/siletto/ms/auth/AppConfiguration.java
@@ -3,58 +3,46 @@ package it.siletto.ms.auth;
 import io.dropwizard.Configuration;
 import io.dropwizard.client.JerseyClientConfiguration;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
 public class AppConfiguration extends Configuration {
-  @Valid
-  @NotNull
-  @JsonProperty
-  private JerseyClientConfiguration httpClient = new JerseyClientConfiguration();
+	@Valid
+	@NotNull
+	@JsonProperty
+	private JerseyClientConfiguration httpClient = new JerseyClientConfiguration();
 
-  @JsonProperty
-  private String proxyHost = null;
+	@JsonProperty
+	private String proxyHost = null;
 
-  @JsonProperty
-  private int proxyPort = 8080;
+	@JsonProperty
+	private int proxyPort = 8080;
 
-  @JsonProperty
-  private HashMap<String, String> adminUsers = null;
+	@JsonProperty
+	private String privateKeyFile = null;
 
-  @JsonProperty
-  private String privateKeyFile = null;
+	@JsonProperty
+	private String publicKeyFile = null;
 
-  @JsonProperty
-  private String publicKeyFile = null;
+	public JerseyClientConfiguration getJerseyClientConfiguration() {
+		return httpClient;
+	}
 
+	public String getProxyHost() {
+		return proxyHost;
+	}
 
-  public JerseyClientConfiguration getJerseyClientConfiguration() {
-    return httpClient;
-  }
+	public int getProxyPort() {
+		return proxyPort;
+	}
 
-  public Map<String, String> getAdminUsers() {
-      return adminUsers;
-  }
-  
-  public String getProxyHost() {
-      return proxyHost;
-  }
-  
-  public int getProxyPort() {
-      return proxyPort;
-  }
+	public String getPrivateKeyFile() {
+		return privateKeyFile;
+	}
 
-public String getPrivateKeyFile() {
-	return privateKeyFile;
-}
-
-public String getPublicKeyFile() {
-	return publicKeyFile;
-}
+	public String getPublicKeyFile() {
+		return publicKeyFile;
+	}
 }
